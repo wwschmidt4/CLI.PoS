334a011bb9a4705f29557b8d857c832f2faab0c9
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2026-02-09 10:11:07 -0500

    Begin process of displaying currently available menu items in the main admin view

--- Maui.PoS/Views/AdminMainView.xaml.cs
+++ Maui.PoS/Views/AdminMainView.xaml.cs
@@ -1,10 +1,21 @@
+using CLI.PoS.Model;
+using Library.PoS.Services;
+
 namespace Maui.PoS.Views;
 
 public partial class AdminMainView : ContentPage
 {
+	public List<Item> Items
+	{
+		get
+		{
+			return ItemServiceProxy.Current.Items;
+		}
+	}
 	public AdminMainView()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 
     private void GoBackClicked(object sender, EventArgs e)
